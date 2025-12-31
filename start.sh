a70a3f16c7f51ede58baabcb2d9d042c5ea870d4
gunman-vagabond <gunman.forever00@gmail.com>
2025-12-31 11:17:35 +0900

    Disable host check in ng serve command

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #GUNCLOCK_API=http://192.168.11.11:3000/gunclock
+#GUNCLOCK_API=http://gunman.mydns.jp:3000/gunclock
 npm run prebuild
-
-ng serve --host 0.0.0.0
+ng serve --host 0.0.0.0 --disable-host-check
